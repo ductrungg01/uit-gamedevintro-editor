c24ad51592c6041e2cfaa36eae02effcffe7f521
Vũ Đức Trung <vuductrung14112001@gmail.com>
2023-09-10 18:03:55 +0700

    hand mouse cursor

--- src/main/java/components/EditorCamera.java
+++ src/main/java/components/EditorCamera.java
@@ -1,5 +1,7 @@
 package components;
 
+import imgui.ImGui;
+import imgui.flag.ImGuiMouseCursor;
 import system.Camera;
 import system.KeyListener;
 import system.MouseListener;
@@ -33,45 +35,33 @@ public class EditorCamera extends Component implements INonAddableComponent {
     //region Override methods
     @Override
     public void editorUpdate(float dt) {
-            if (MouseListener.mouseButtonDown(GLFW_MOUSE_BUTTON_RIGHT) && dragDebounce > 0) {
-                this.clickOrigin = MouseListener.getWorld();
-                dragDebounce -= dt;
-                return;
-            } else if (MouseListener.mouseButtonDown(GLFW_MOUSE_BUTTON_RIGHT)) {
-                Vector2f mousePos = MouseListener.getWorld();
-                Vector2f delta = new Vector2f(mousePos).sub(this.clickOrigin);
-
-                levelEditorCamera.position.sub(delta.mul(dt).mul(dragSensitivity));
-                this.clickOrigin.lerp(mousePos, dt);
+        if (MouseListener.mouseButtonDown(GLFW_MOUSE_BUTTON_RIGHT)){
+            if (ImGui.getMouseCursor() != ImGuiMouseCursor.Hand){
+                ImGui.setMouseCursor(ImGuiMouseCursor.Hand);
             }
-            if (dragDebounce <= 0.0f && !MouseListener.mouseButtonDown(GLFW_MOUSE_BUTTON_RIGHT)) {
-                dragDebounce = 0.1f;
-            }
-
-            if (MouseListener.getScrollY() != 0.0f) {
-                float addValue = (float) Math.pow(Math.abs(MouseListener.getScrollY() * scrollSensivity),
-                        1 / levelEditorCamera.getZoom());
-
-                addValue *= -Math.signum(MouseListener.getScrollY());
-                levelEditorCamera.addZoom(addValue);
-            }
-
-        if (KeyListener.isKeyPressed(GLFW_KEY_KP_DECIMAL)) {
-            reset = true;
         }
 
-        if (reset) {
-            levelEditorCamera.position.lerp(new Vector2f(), lerpTime);
-            levelEditorCamera.setZoom(this.levelEditorCamera.getZoom() +
-                    ((1.0f - levelEditorCamera.getZoom()) * lerpTime));
-            this.lerpTime += 0.1f * dt;
-            if (Math.abs(levelEditorCamera.position.x) <= 5.0f &&
-                    Math.abs(levelEditorCamera.position.y) <= 5.0f) {
-                this.lerpTime = 0;
-                levelEditorCamera.position.set(0f, 0f);
-                this.levelEditorCamera.setZoom(1.0f);
-                reset = false;
-            }
+        if (MouseListener.mouseButtonDown(GLFW_MOUSE_BUTTON_RIGHT) && dragDebounce > 0) {
+            this.clickOrigin = MouseListener.getWorld();
+            dragDebounce -= dt;
+            return;
+        } else if (MouseListener.mouseButtonDown(GLFW_MOUSE_BUTTON_RIGHT)) {
+            Vector2f mousePos = MouseListener.getWorld();
+            Vector2f delta = new Vector2f(mousePos).sub(this.clickOrigin);
+
+            levelEditorCamera.position.sub(delta.mul(dt).mul(dragSensitivity));
+            this.clickOrigin.lerp(mousePos, dt);
+        }
+        if (dragDebounce <= 0.0f && !MouseListener.mouseButtonDown(GLFW_MOUSE_BUTTON_RIGHT)) {
+            dragDebounce = 0.1f;
+        }
+
+        if (MouseListener.getScrollY() != 0.0f) {
+            float addValue = (float) Math.pow(Math.abs(MouseListener.getScrollY() * scrollSensivity),
+                    1 / levelEditorCamera.getZoom());
+
+            addValue *= -Math.signum(MouseListener.getScrollY());
+            levelEditorCamera.addZoom(addValue);
         }
     }
 
